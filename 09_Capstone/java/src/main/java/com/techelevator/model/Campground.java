17df52c6b46eacdc64a6f004ffd45cb1ab17d2e6
nathantyler <56833450+nathantyler@users.noreply.github.com>
2019-10-24 15:02:18 -0400

    Changed some stuff in Campground.

--- 09_Capstone/java/src/main/java/com/techelevator/model/Campground.java
+++ 09_Capstone/java/src/main/java/com/techelevator/model/Campground.java
@@ -1,81 +1,128 @@
 package com.techelevator.model;
 
 import java.math.BigDecimal;
-import java.util.List;
 
-
-public class Campground implements CampgroundDAO {
-
-	private long campground_ID;
-	private long park_ID;
-	private String campGroundName;
-	private String openMonth;
-	private String closedMonth;
+public class Campground {
+	
+	// I had to change some of these variable names
+	// as I know they were gonna leave to confusion down the line
+	// I try to keep them as close to as possible to the database column names
+	// The underscores were just an "off-convention" thing.
+	private int campgroundId;
+	private int parkId;
+	private String name;
+	private String openFrom;
+	private String openTo;
 	private BigDecimal dailyFee;
 
- /* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  * The above satisfies requirement 2a= A campground includes an id, name,
-  *  open month, closing month, and a daily fee. 
-  *  
-  *  Below are the getters and setters 
-  *  
-  */
+	/*
+	 * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ The above satisfies requirement 2a=
+	 * A campground includes an id, name, open month, closing month, and a daily
+	 * fee.
+	 * 
+	 * Below are the getters and setters
+	 * 
+	 */
+
 	
+
 	
-	public long getCampground_ID() {
-		return campground_ID;
-	}
-	public void setCampgroundId(long campground_ID) {
-		this.campground_ID = campground_ID;
-	}
-	public long getParkId() {
-		return park_ID;
-	}
-	public void setParkId(long park_ID) {
-		this.park_ID = park_ID;
-	}
-	public String getCampGroundName() {
-		return campGroundName;
-	}
-	public void setName(String campGroundName) {
-		this.campGroundName = campGroundName;
-	}
-	public String getOpenMonth() {
-		return openMonth;
-	}
-	public void setOpen(String openMonth) {
-		this.openMonth = openMonth;
-	}
-	public String getClosedMonth() {
-		return closedMonth;
-	}
-	public void setClosedMonth(String closedMonth) {
-		this.closedMonth = closedMonth;
-	}
-	public BigDecimal getDailyFee() {
-		
-		return dailyFee;
-	}
-	public void setFee(BigDecimal dailyFee) {
-		this.dailyFee = dailyFee.setScale(2);
-	}
-	@Override
-	
-	//In case we need to print out messages using strings and bigdecimal
-	public String toString() {
-		return campGroundName +"   "+ openMonth + "    "+ closedMonth+"    "+dailyFee;
-	}
-	
-	// 2 methods need to be written
-	@Override
-	public List<Campground> getAllCampgrounds(long park_ID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public List<Campground> getCampgroundInfoByPark(long choice) {
-		// TODO Auto-generated method stub
-		return null;
+
+	// In case we need to print out messages using strings and bigdecimal
+	/**
+	 * @return the campgroundId
+	 */
+	public int getCampgroundId() {
+		return campgroundId;
 	}
 
+	/**
+	 * @return the parkId
+	 */
+	public int getParkId() {
+		return parkId;
 	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the openFrom
+	 */
+	public String getOpenFrom() {
+		return openFrom;
+	}
+
+	/**
+	 * @return the openTo
+	 */
+	public String getOpenTo() {
+		return openTo;
+	}
+
+	/**
+	 * @return the dailyFee
+	 */
+	public BigDecimal getDailyFee() {
+		return dailyFee;
+	}
+
+	/**
+	 * @param campgroundId the campgroundId to set
+	 */
+	public void setCampgroundId(int campgroundId) {
+		this.campgroundId = campgroundId;
+	}
+
+	/**
+	 * @param parkId the parkId to set
+	 */
+	public void setParkId(int parkId) {
+		this.parkId = parkId;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @param openFrom the openFrom to set
+	 */
+	public void setOpenFrom(String openFrom) {
+		this.openFrom = openFrom;
+	}
+
+	/**
+	 * @param openTo the openTo to set
+	 */
+	public void setOpenTo(String openTo) {
+		this.openTo = openTo;
+	}
+
+	/**
+	 * @param dailyFee the dailyFee to set
+	 */
+	public void setDailyFee(BigDecimal dailyFee) {
+		this.dailyFee = dailyFee;
+	}	
+	
+	// The column daliy_fee in the database uses the Postgres datatype "money".
+	// Java doesn't have a direct analog. As such, passing the dailyFee as String is
+	// probably going to be helpful.
+	public void setDailyFee(String dailyFee) {
+		this.dailyFee = new BigDecimal(dailyFee);
+	}
+
+	@Override
+	public String toString() {
+		return name + "   " + openTo + "    " + openTo + "    " + dailyFee;
+	}
+
+}
