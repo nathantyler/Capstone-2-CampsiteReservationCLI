e8e43ed28324f432c149329dece129a8be4eff74
nathantyler <56833450+nathantyler@users.noreply.github.com>
2019-10-24 09:44:20 -0400

    Removed the hardcoding of the URL and login information to some finals

--- 09_Capstone/java/src/main/java/com/techelevator/CampgroundCLI.java
+++ 09_Capstone/java/src/main/java/com/techelevator/CampgroundCLI.java
@@ -5,12 +5,17 @@ import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 public class CampgroundCLI {
+	
+	private static final String DATABASE_NAME = "campground";
+	private static final String URL = "jdbc:postgresql://localhost:5432/" + DATABASE_NAME;
+	private static final String USERNAME = "postgres";
+	private static final String PASSWORD = "postgres1";
 
 	public static void main(String[] args) {
 		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setUrl("jdbc:postgresql://localhost:5432/campground");
-		dataSource.setUsername("postgres");
-		dataSource.setPassword("postgres1");
+		dataSource.setUrl(URL);
+		dataSource.setUsername(USERNAME);
+		dataSource.setPassword(PASSWORD);
 
 		CampgroundCLI application = new CampgroundCLI(dataSource);
 		application.run();
